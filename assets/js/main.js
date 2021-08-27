3ed3379f7a85d62202f3d8a2bd29bb28f51d8020
shijo124 <at.shijo+github@protonmail.com>
2021-08-28 00:36:11 +0900

    shijo edit 12

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,6 +124,6 @@ function debug(){
 }
 
 function cookie_del(){
-    $.cookie("bonus_data", null);
-    $.cookie("bonus_rate", null);
+    $.cookie("bonus_data", "", {path:'/', expires: -1});
+    $.cookie("bonus_rate", "", {path:'/', expires: -1});
 }

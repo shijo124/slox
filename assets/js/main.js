3997c15a783f174461368b7ba40158154679b03e
shijo124 <at.shijo+github@protonmail.com>
2021-08-28 00:47:24 +0900

    shijo edit 16

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,8 +124,8 @@ function debug(){
 }
 
 function cookie_del(){
-    reset_bonus_data = [];
-    reset_bonus_rate = {};
+    let reset_bonus_data = [];
+    let reset_bonus_rate = {};
     com_set_cookie('bonus_data', reset_bonus_data, {path:'/'});
     com_set_cookie('bonus_rate', reset_bonus_rate, {path:'/'});
 }
